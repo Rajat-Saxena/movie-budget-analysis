dee183ddc969fa3c896d3b35ad1f93a2e7fc9d96
Rajat Saxena <rajatsaxena93@gmail.com>
2018-06-01 23:26:58 +0530

    Added Spark dependencies

--- src/main/scala/com/rajat/saxena/App.scala
+++ src/main/scala/com/rajat/saxena/App.scala
@@ -1,9 +1,13 @@
 package com.rajat.saxena
 
-object App {
+import org.apache.spark.{SparkConf, SparkContext}
 
+object App {
   def main(args: Array[String]) {
-    //val conf = new SparkConf().setAppName("Movie Budget Analysis").setMaster("local")
-    println("Hello world")
+    val conf = new SparkConf().setAppName("Movie Budget Analysis").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val movieData = sc.textFile("src/main/resources/AllMoviesDetailsCleaned.csv")
+    println("Row count:" + movieData.count())
   }
 }
