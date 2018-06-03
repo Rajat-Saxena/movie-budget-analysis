c79d355c333b7505bda7882c2b554f1bc319b918
Rajat Saxena <rajatsaxena93@gmail.com>
2018-06-03 13:15:52 +0530

    Added code for use case #1

--- src/main/scala/com/rajat/saxena/App.scala
+++ src/main/scala/com/rajat/saxena/App.scala
@@ -1,21 +1,27 @@
 package com.rajat.saxena
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object App {
 
   def main(args: Array[String]) {
+    val appStartTime = System.nanoTime
+
     val conf = new SparkConf().setAppName("Movie Budget Analysis").setMaster("local")
     val sc = new SparkContext(conf)
 
     // http://www.usinflationcalculator.com/inflation/consumer-price-index-and-annual-percent-changes-from-1913-to-2008/
     val cpiData = sc.textFile("src/main/resources/CPI.txt")
         .map(_.split("\t"))
-        .map(row => (row(0).trim.toInt, row(13).trim.toFloat))
-    val cpiBroadcast = sc.broadcast(cpiData.collect())
+        .map(row => (row(0).trim, row(13).trim.toFloat))
+    val cpiBroadcast = sc.broadcast(cpiData.collectAsMap())
     val cpi2018 = 250.546
 
     val movieData = sc.textFile("src/main/resources/AllMoviesDetailsCleaned.csv")
+      .map(_.split(";(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)"))
+      .filter(row => row(10).length == 10)
+      .filter(row => row(14) == "Released")
 
     /* Objective:
      - (adjust budget for inflation)
@@ -27,28 +33,35 @@ object App {
      */
 
     // Get movie title and budget from main data
-
     val header = movieData.first()
     val convertedBudgets = movieData
       .filter(_ != header)
-      .map(_.split(";(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)"))
-      .filter(row => row(10) != null)
-      .map(row => ((((cpi2018 - cpiBroadcast.value.toMap.get(row(10).substring(6,10).toInt).get)
-        /cpiBroadcast.value.toMap.get(row(10).substring(6,10).toInt).get) + 1) * row(1).toInt).toInt)
-    convertedBudgets.take(20).foreach(println)
+      .map(row => ((((cpi2018 - cpiBroadcast.value.getOrElse(row(10).substring(6,10), 9.9.toFloat))
+      /cpiBroadcast.value.getOrElse(row(10).substring(6,10), 9.9.toFloat)) + 1) * row(1).toInt).toInt)
+    //convertedBudgets.foreach(println)
 
     val movieDataCleaned = movieData
       .filter(_ != header)
-      .map(_.split(";(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)"))
-      .filter(row => row(10) != null)
       .zip(convertedBudgets)
-      .map(row => (row._1(5),             // Title
-        row._1(10).substring(6,10).toInt, // Release Year
-        row._1(17),                       // Avg Rating
-        row._1(1).toInt,                  // Budget
-        row._2                            // Converted Budget
+      .map(row => (row._1(16),             // String - Title
+        row._1(10).substring(6,10).toInt, // Int - Release Year
+        row._1(17).toFloat,               // Float - Avg Rating
+        row._1(1).toInt,                  // Int - Budget
+        row._2                            // Int - Converted Budget
       ))
 
-    movieDataCleaned.take(20).foreach(println)
+    // Find most expensive movies
+    mostExpensiveMoviesOfAllTime(movieDataCleaned)
+    //movieDataCleaned.take(20).foreach(println)
+
+    val duration = (System.nanoTime - appStartTime) / 1e9d
+    println("Program executed for: " + duration + " seconds")
+  }
+
+  def mostExpensiveMoviesOfAllTime(movieDataCleaned: RDD[(String, Int, Float, Int, Int)]) = {
+
+    val mostExpensiveMoviesOfAllTime = movieDataCleaned.sortBy(movie => (movie._5, movie._3), ascending = false)
+
+    mostExpensiveMoviesOfAllTime.take(25).foreach(println)
   }
 }
